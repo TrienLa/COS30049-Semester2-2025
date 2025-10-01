62b9a27927c4227ce38e7a8de5b95a1999d4954f
TrienLa <104350343@student.swin.edu.au>
2025-10-02 00:58:54 +1000

    Update more visualization

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import plotly.express as pex
+import plotly.io
 import os, sys, re
 
 def load_data(filename):
@@ -22,16 +23,32 @@ def load_data(filename):
         print(f"Error loading data: {e}")
         return None
 
-def generate_graph(email_df):
+def generate_feature_plot(email_df):
     """
     Args:
-        email_df (panda DataFrame): DataFrame containing information from CSV file.
+        email_df (pandas DataFrame): DataFrame containing information from CSV file.
     """
+    # Generate plot for distribution of spam vs ham in the dataset 
     spam_counts = email_df['spam'].value_counts()
-    fig = pex.bar(spam_counts, x=spam_counts.index, y=spam_counts.values, labels={'x': 'Spam', 'y': 'Count'}, title='Distribution of Spam vs Ham Emails')
-    fig.show()
+    spam_distribution = pex.bar(spam_counts, x=spam_counts.index, y=spam_counts.values, labels={'x': 'Spam', 'y': 'Count'}, title='Distribution of Spam vs Ham emails')
+    plotly.io.write_image(spam_distribution, sys.path[0] + "/plots/spam_distribution.png")
+
+    # Generate plot for text length distribution of spam vs ham in the dataset 
+    email_df['text length'] = email_df['text'].apply(len) # Add a new data column categorizing the length of each email
+    email_length = pex.histogram(email_df, y='text length', color='spam', nbins=35, title='Text length distribution by Spam vs Ham')
+    plotly.io.write_image(email_length, sys.path[0] + "/plots/email_length.png")
+
+    # Generate a word cloud for non spam emails
+
+    # Generate a word cloud for spam emails
+
+    # Generate a histogram of keyword frequencies
+
+    
 
 if __name__ == "__main__":
-    # 1. Load the email data.
+    # Load the email data
     email_dfs = load_data(sys.path[0] + "/dataset/emails.csv")
-    generate_graph(email_dfs)
+
+    # Generate plot
+    generate_feature_plot(email_dfs)
