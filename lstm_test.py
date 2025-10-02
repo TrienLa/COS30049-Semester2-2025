2455e0dd7dc6c6ff51756172260583fc403ad27a
notAKAI <103517891@student.swin.edu.au>
2025-10-03 02:03:28 +1000

    lstm test model

--- lstm_test.py
+++ lstm_test.py
@@ -1,6 +1,7 @@
 import keras.models as kmodels
+import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import TextVectorization
+from keras.layers import TextVectorization, Normalization
 from keras.layers import Embedding, LSTM, Dense
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
@@ -17,33 +18,35 @@ def generate_model(email_df):
     """
     # Tokenizer and pad sequences for model
     vectorizer = TextVectorization(
-        max_tokens=10000,
+        max_tokens=1000,
         output_mode='int',
-        output_sequence_length=100,      # Ensure all sequences have the same length
+        output_sequence_length=20,      # Ensure all sequences have the same length
         standardize='lower_and_strip_punctuation'   # Lowercase and split by whitespace
     )
     vectorizer.adapt(email_df['text'])
-    vectorized = vectorizer(email_df['text'])
-    X = vectorized.numpy()
-    y = to_categorical(email_df['spam'])
+    
+    
 
-    # Split data for training and testing
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Create dataset
+    ds = tf.data.Dataset.from_tensor_slices((tf.constant(email_df['text']),tf.constant(email_df['spam'])))
+    ds = ds.shuffle(buffer_size=8).batch(2).prefetch(tf.data.AUTOTUNE)
 
     # Model buidling and compiling
     lstm = Sequential([
-        Embedding(input_dim=100,output_dim=128),
+        vectorizer,
+        Embedding(input_dim=1000,output_dim=64),
         LSTM(128),
-        Dense(2, activation='sigmoid')
+        Dense(64, activation='relu'),
+        Dense(1, activation='sigmoid')
         ])
 
     lstm.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # Model training
-    lstm.fit(X_train, y_train, epochs=10, batch_size=64, validation_split=0.2)
+    lstm.fit(ds, epochs=10)
 
     # Model evaluation
-    loss, accuracy = lstm.evaluate(X_test, y_test)
+    loss, accuracy = lstm.evaluate(ds)
     print('LSTM Loss:', loss)
     print('LSTM Accuracy:',accuracy)
 
